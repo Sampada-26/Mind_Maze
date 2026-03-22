25e1790f10e74016bf1bba400dabf7d53ebc7ecb
Sampada Kaginkar <kaginkarsampada@gmail.com>
2026-03-22 18:22:08 +0530

    nothing

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/sampada-kaginkar/Pictures/gamee/venv
+set -gx VIRTUAL_ENV /home/sampada-kaginkar/Pictures/gameeeeee/Mind_Maze/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
